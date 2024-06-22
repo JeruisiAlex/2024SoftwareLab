a83176ca8a91f3ca4ab1afe7ae92d52b959f5c78
JeruisiAlex <1525772623@qq.com>
2024-06-22 17:38:09 +0800

    20240622 1738 Jeruisi

--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
+import './assets/element.scss'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app = createApp(App)
+app.use(store).use(router).use(ElementPlus)
+app.mount('#app')
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
